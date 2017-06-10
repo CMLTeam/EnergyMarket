6f74580f556af2de43e09a34394512b800877afa
xonix <xonixx@gmail.com>
2017-06-10 14:11:47 +0300

    init

---
+++ src/main/java/com/cmlteam/energymarket/EnergymarketApplication.java
@@ -0,0 +1,12 @@
+package com.cmlteam.energymarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergymarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergymarketApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cmlteam/energymarket/EnergymarketApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cmlteam.energymarket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EnergymarketApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

1d807f16aaf63704eb7aaceaeab9be47667e574f
xonix <xonixx@gmail.com>
2017-06-10 15:10:01 +0300

    ctrl

--- src/main/java/com/cmlteam/energymarket/EnergymarketApplication.java
+++ src/main/java/com/cmlteam/energymarket/EnergymarketApplication.java
@@ -1,11 +1,17 @@
 package com.cmlteam.energymarket;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.cmlteam.energymarket")
+@Configuration
+@EnableAutoConfiguration
 public class EnergymarketApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EnergymarketApplication.class, args);
 	}

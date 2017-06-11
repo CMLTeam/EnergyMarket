ec5aff3576801ce689e8f013b9a83fd98e3dedb9
xonix <xonixx@gmail.com>
2017-06-11 15:01:39 +0300

    energy market contract

--- eth/vlad_experiment/contracts/EnergyMarketContract.sol
+++ eth/vlad_experiment/contracts/EnergyMarketContract.sol
@@ -16,6 +16,8 @@ contract EnergyMarketContract {
 
     // TODO: conversion rate Eth -> EnergyMarket
     uint32 CONVERSION_RATE = 1;
+    // TODO: configurable
+    uint32 ALLOWENCE_INTERVAL_KWH = 10;
 
     struct MonthEnergyProposal {
         uint8 month;
@@ -27,6 +29,7 @@ contract EnergyMarketContract {
     struct MonthEnergyCommitment {
         uint8 month;
         address consumer;
+        address producer;
         uint128 commitedVolume;
     }
 
@@ -42,7 +45,8 @@ contract EnergyMarketContract {
             consumersBalances[consumer] += CONVERSION_RATE * msg.value;
             return true;
         } else {
-            ErrorLog(consumer, "Alredy has proposal for month");
+            ErrorLog(consumer, "Can't refill");
+            return false;
         }
     }
 
@@ -58,7 +62,7 @@ contract EnergyMarketContract {
 
         // if this month was already submitted
         if (current.month != 0) {
-            ErrorLog(producer, "Alredy has proposal for month");
+            ErrorLog(producer, "Already has proposal for month");
             return false;
         }
 
@@ -86,10 +90,38 @@ contract EnergyMarketContract {
         commitment.month = month;
         commitment.consumer = consumer;
         commitment.commitedVolume = commitedVolume;
+        commitment.producer = producerToBuyFrom;
         commitsByConsumer[consumer][month] = commitment;
 
         proposal.volume -= commitedVolume;
         proposalsByProducer[producerToBuyFrom][month] = proposal;
         return true;
-    } 
+    }
+
+    // Executed at the month end
+    function closeMonth(address consumer, uint8 month, uint128 realConsumedVolume) payable returns (bool sucess) {
+        MonthEnergyCommitment memory commitment = commitsByConsumer[consumer][month];
+        address producer = commitment.producer;
+        MonthEnergyProposal memory proposal = proposalsByProducer[producer][month];
+
+        uint256 consumerToPay;
+
+        if (realConsumedVolume < commitment.commitedVolume - ALLOWENCE_INTERVAL_KWH) {
+            consumerToPay = proposal.priceBeyondInterval * realConsumedVolume;
+        } else if (realConsumedVolume > commitment.commitedVolume - ALLOWENCE_INTERVAL_KWH) {
+            uint256 fee = proposal.priceBeyondInterval * (realConsumedVolume - commitment.commitedVolume);
+            consumerToPay = proposal.price * realConsumedVolume + fee;
+        }
+
+        consumersBalances[consumer] -= consumerToPay;
+    
+        // transfer Eth to producer
+        // TODO usage fee
+        if (producer.send(consumerToPay / CONVERSION_RATE)) {
+            return true;
+        } else {
+            return false;
+            ErrorLog(producer, "Can't process transaction");
+        }
+    }
 }

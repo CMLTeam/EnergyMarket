b70d9943d9536a2238cbde43a98cb19b4ac5e6c2
xonix <xonixx@gmail.com>
2017-06-11 14:35:31 +0300

    energy market contract

--- eth/vlad_experiment/contracts/EnergyMarketContract.sol
+++ eth/vlad_experiment/contracts/EnergyMarketContract.sol
@@ -5,24 +5,45 @@ pragma solidity ^0.4.11;
 // between energy consumers & producers
 //
 contract EnergyMarketContract {
-    mapping(address => uint64) consumersBalances;
+    mapping(address => uint256) consumersBalances;
 
     mapping(address => mapping(uint8 => MonthEnergyProposal)) proposalsByProducer;
+    mapping(address => mapping(uint8 => MonthEnergyCommitment)) commitsByConsumer;
+
+    address owner;
 
     event ErrorLog(address actor, string errorMsg);
+
+    // TODO: conversion rate Eth -> EnergyMarket
+    uint32 CONVERSION_RATE = 1;
 
     struct MonthEnergyProposal {
         uint8 month;
         uint128 volume;
         uint32 price;
+        uint32 priceBeyondInterval;
+    }
+
+    struct MonthEnergyCommitment {
+        uint8 month;
+        address consumer;
+        uint128 commitedVolume;
     }
 
     function EnergyMarketContract() {
+        owner = msg.sender;
     }
 
     // refill with Eth
-    function refillConsumerBalance() payable {
+    function refillConsumerBalance() payable returns (bool sucess) {
+        address consumer = msg.sender;
 
+        if(owner.send(msg.value)) {
+            consumersBalances[consumer] += CONVERSION_RATE * msg.value;
+            return true;
+        } else {
+            ErrorLog(consumer, "Alredy has proposal for month");
+        }
     }
 
     function addEnergyProposal(uint8 month, uint128 volume, uint32 price) returns (bool success) {
@@ -45,7 +66,9 @@ contract EnergyMarketContract {
         return true;
     }
 
-    function prepayConsumedEnergy(address producerToBuyFrom, uint8 month, uint128 requestedVolume) returns (bool success) {
+    // Consumer in advance commits to consume (& so pay afterwards) specifies amount of energy (in interval).
+    // If he violates this commitment - he will be suject to additional fee as set by producer
+    function commitToConsumeEnergy(address producerToBuyFrom, uint8 month, uint128 commitedVolume) returns (bool success) {
         address consumer = msg.sender;
         MonthEnergyProposal memory proposal = proposalsByProducer[producerToBuyFrom][month];
 
@@ -54,12 +77,18 @@ contract EnergyMarketContract {
             return false;
         }
 
-        if (proposal.volume < requestedVolume) {
+        if (proposal.volume < commitedVolume) {
             ErrorLog(consumer, "Not enough energy for given month");
             return false;
         }
 
-        proposal.volume -= requestedVolume;
+        MonthEnergyCommitment memory commitment;
+        commitment.month = month;
+        commitment.consumer = consumer;
+        commitment.commitedVolume = commitedVolume;
+        commitsByConsumer[consumer][month] = commitment;
+
+        proposal.volume -= commitedVolume;
         proposalsByProducer[producerToBuyFrom][month] = proposal;
         return true;
     } 
